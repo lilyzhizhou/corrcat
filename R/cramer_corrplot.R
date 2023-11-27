d53f5395d7038f16d7534b57cbcb07236e0f615f
Zhi Lin Zhou <lilyzhi.zhou@mail.utoronto.ca>
2023-11-27 12:07:21 -0500

    color update

--- R/cramer_corrplot.R
+++ R/cramer_corrplot.R
@@ -14,5 +14,5 @@ cramer_corrplot <- function(df) {
   cramer_matrix <- calculate_cramers_v(df)
   cramer_matrix
 
-  corrplot::corrplot(cramer_matrix, method = "color", addCoef.col = "black", is.corr = FALSE, tl.col = "black")
+  corrplot::corrplot(cramer_matrix, method = "color", addCoef.col = "black", is.corr = FALSE, tl.col = "black", col = 'YlGnBu')
 }
