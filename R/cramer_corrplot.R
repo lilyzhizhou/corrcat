4df9dae1b17f04dfa8736432291e013c0bc0eef8
Zhi Lin Zhou <lilyzhi.zhou@mail.utoronto.ca>
2023-11-27 16:02:23 -0500

    col

--- R/cramer_corrplot.R
+++ R/cramer_corrplot.R
@@ -14,5 +14,5 @@ cramer_corrplot <- function(df) {
   cramer_matrix <- calculate_cramers_v(df)
   cramer_matrix
 
-  corrplot::corrplot(cramer_matrix, method = "color", addCoef.col = "black", is.corr = FALSE, tl.col = "black", col = COL1('Purple', 10))
+  corrplot::corrplot(cramer_matrix, method = "color", addCoef.col = "black", is.corr = FALSE, tl.col = "black", col = corrplot::COL1('Purple', 10))
 }
